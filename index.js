e2412a5cb32c8c8b5a85f8e05db23b771e78c2de
ahmadqaranyfarhan <ahmadqaranyfarhan5@gmail.com>
2024-04-19 16:23:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array, giftType) {
+  let thanksMessages = [];
+  for (let i = 0; i < array.length; i++) {
+    thanksMessages.push(
+      `Thank you, ${array[i]}, for the wonderful ${giftType} gift!`
+    );
+  }
+  return thanksMessages;
+}
+
+writeCards(["Elaf", "Amina", "Abdulrahman"], "Birthday");
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+countDown(10);
